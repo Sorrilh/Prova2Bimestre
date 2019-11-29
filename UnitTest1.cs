82de0fb57cea826d1b69d33c5f509cf14617062e
Renato Jr <renato.sorrilha@senaimt.edu.br>
2019-11-28 21:00:41 -0400

    #Commit - Branch develop

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Prova2Bimestre_1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+//Commit realizado com sucesso
+        }
+    }
+}
